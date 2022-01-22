d9b1b4d2f4c579d89c79a6cd0fc71e0936136443
scarmain <scarmain@gmail.com>
2022-01-21 20:34:19 -0500

    Add customizable driver stick tuning

--- src/main/java/frc/robot/commands/DriveStick.java
+++ src/main/java/frc/robot/commands/DriveStick.java
@@ -23,10 +23,32 @@ public class DriveStick extends CommandBase {
     public void execute() {
         //axis 0 = left stick left/right, 1 = left stick up/down, 4 = right stick left/right
 
-        double xSpeed = Drivetrain.deadband(m_stick.getRawAxis(0));
-        double ySpeed = Drivetrain.deadband(m_stick.getRawAxis(1));
-        double rot = Drivetrain.deadband(m_stick.getRawAxis(4));
+        double xSpeed = curveDriveStick(deadband(m_stick.getRawAxis(0)));
+        double ySpeed = curveDriveStick(deadband(m_stick.getRawAxis(1)));
+        double rot = curveDriveStick(deadband(m_stick.getRawAxis(4)));
         
         drive.drive(xSpeed, -ySpeed, rot, false);
+    }
+
+    private static double curveDriveStick (double input) {
+        final double a = 0.5;       //if a=0, driver input is linear, if a=1, driver input cubed, 0.5 = squared (roughly)
+        final double b = 0;       //deadband constant
+
+        if(input >=0) {
+            return b + (1-b)*(a*Math.pow(input, 3) + (1-a)*input);
+        } else {
+            return -b + (1-b)*(a*Math.pow(input, 3) + (1-a)*input);
+        }
+    }
+
+    private static double deadband(double input) {
+        final double DEADBAND = 0.13; 
+        double abs = Math.abs(input);
+
+        if (abs > DEADBAND) {
+            return Math.signum(input) * ((abs-DEADBAND)/(1-DEADBAND));
+        } else {
+            return 0;
+        }
     }
 }
