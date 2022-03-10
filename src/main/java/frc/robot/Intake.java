3cedd4eca8f781dad5e4a72242a64559270163ad
scarmain <scarmain@gmail.com>
2022-03-10 16:04:24 -0500

    Add color sensor

--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -16,13 +18,26 @@ public class Intake extends SubsystemBase {
     private CANSparkMax intakeMotor;
     private CANSparkMax upMotor;
     private ColorSensorV3 colorSensor;
+    private ColorMatch m_colorMatcher = new ColorMatch();
+    private final Color kBlueTarget = new Color(0.171, 0.421, 0.406);
+    private final Color kRedTarget = new Color(0.499, 0.362, 0.138);
+
+    private enum CargoColor {
+        Red,
+        Blue,
+        Unknown
+    }
 
     public Intake() {
         intakeMotor = new CANSparkMax(24,MotorType.kBrushless);
         intakeMotor.setInverted(true);
+        
         upMotor = new CANSparkMax(31,MotorType.kBrushless);
         upMotor.setInverted(true);
+        
         colorSensor = new ColorSensorV3(Port.kOnboard);
+        m_colorMatcher.addColorMatch(kBlueTarget);
+        m_colorMatcher.addColorMatch(kRedTarget);
     }
 
     public void setIntake(double voltPct) {
@@ -36,9 +51,21 @@ public class Intake extends SubsystemBase {
     @Override
     public void periodic() {
         SmartDashboard.putNumber("Intake Speed%", intakeMotor.get());
+
         Color color = colorSensor.getColor();
+        ColorMatchResult match = m_colorMatcher.matchClosestColor(color);
+        CargoColor colorEnum;
+        if (match.color == kBlueTarget) {
+            colorEnum = CargoColor.Blue;
+        } else if (match.color == kRedTarget) {
+            colorEnum = CargoColor.Red;
+        } else {
+            colorEnum = CargoColor.Unknown;
+        }
+
         SmartDashboard.putNumber("Sense Red", color.red);
         SmartDashboard.putNumber("Sense Green", color.green);
         SmartDashboard.putNumber("Sense Blue", color.blue);
+        SmartDashboard.putString("Ball Color", colorEnum.toString());
     }
 }
