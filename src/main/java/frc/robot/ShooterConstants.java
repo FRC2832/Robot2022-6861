72f8f05bffabffcae17ac6123fe019cc44279575
scarmain <scarmain@gmail.com>
2022-03-11 23:57:41 -0500

    Implemented Shooter tables

--- src/main/java/frc/robot/ShooterConstants.java
+++ src/main/java/frc/robot/ShooterConstants.java
@@ -9,22 +9,28 @@ public class ShooterConstants {
     public final static ArrayList<Pair<Double,Double>> DIST_HOOD_TABLE = new ArrayList<Pair<Double,Double>>();
 
     public static void LoadConstants() {
-        //table is input: pixel Y, output: mm from target
-        VISION_DIST_TABLE.add(new Pair<Double, Double>(94.0, 7.7724));
-        VISION_DIST_TABLE.add(new Pair<Double, Double>(245.0, 2.667));
-        VISION_DIST_TABLE.add(new Pair<Double, Double>(298.0, 2.1336));
-        VISION_DIST_TABLE.add(new Pair<Double, Double>(372.0, 1.651));
+        //table is input: pixel Y, output: inches from target
+        VISION_DIST_TABLE.add(new Pair<Double, Double>(54.5, 70.));
+        VISION_DIST_TABLE.add(new Pair<Double, Double>(111., 85.));
+        VISION_DIST_TABLE.add(new Pair<Double, Double>(186.1, 107.));
+        VISION_DIST_TABLE.add(new Pair<Double, Double>(233.25, 127.));
+        VISION_DIST_TABLE.add(new Pair<Double, Double>(270.3, 145.));
+        VISION_DIST_TABLE.add(new Pair<Double, Double>(299.3, 165.));
 
-        //table is input: distance in mm, output: rpm
-        DIST_RPM_TABLE.add(new Pair<Double, Double>(1000d, 2350d));
-        DIST_RPM_TABLE.add(new Pair<Double, Double>(1300d, 2600d));
-        DIST_RPM_TABLE.add(new Pair<Double, Double>(1700d, 2400d));
-        DIST_RPM_TABLE.add(new Pair<Double, Double>(2000d, 2550d));
+        //table is input: distance in inches, output: rpm
+        DIST_RPM_TABLE.add(new Pair<Double, Double>(70., 2150d));
+        DIST_RPM_TABLE.add(new Pair<Double, Double>(85., 2100d));
+        DIST_RPM_TABLE.add(new Pair<Double, Double>(107., 2400d));
+        DIST_RPM_TABLE.add(new Pair<Double, Double>(127., 2369d));
+        DIST_RPM_TABLE.add(new Pair<Double, Double>(145., 2450d));
+        DIST_RPM_TABLE.add(new Pair<Double, Double>(165., 2600d));
 
-        //table is input: distance in mm, output: angle in degrees
-        DIST_HOOD_TABLE.add(new Pair<Double, Double>(1000d, 20d));
-        DIST_HOOD_TABLE.add(new Pair<Double, Double>(1300d, 30d));
-        DIST_HOOD_TABLE.add(new Pair<Double, Double>(1700d, 25d));
-        DIST_HOOD_TABLE.add(new Pair<Double, Double>(2000d, 42d));
+        //table is input: distance in inches, output: angle in degrees
+        DIST_HOOD_TABLE.add(new Pair<Double, Double>(70.,  21.375));
+        DIST_HOOD_TABLE.add(new Pair<Double, Double>(85.,  28.375));
+        DIST_HOOD_TABLE.add(new Pair<Double, Double>(107., 25.125));
+        DIST_HOOD_TABLE.add(new Pair<Double, Double>(127., 34.25));
+        DIST_HOOD_TABLE.add(new Pair<Double, Double>(145., 34.875));
+        DIST_HOOD_TABLE.add(new Pair<Double, Double>(165., 42.125));
     }
 }
