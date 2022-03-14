691cb7e6cd6c9c38a63d1e7aca059c39ca62a0b0
scarmain <scarmain@gmail.com>
2022-03-13 22:09:32 -0400

    Added turret soft limits to stop the turret from going too far

--- src/main/java/frc/robot/Turret.java
+++ src/main/java/frc/robot/Turret.java
@@ -1,7 +1,11 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Turret extends SubsystemBase {
@@ -9,9 +13,24 @@ public class Turret extends SubsystemBase {
 
     public Turret() {
         turretMotor = new CANSparkMax(30,MotorType.kBrushless);
+        turretMotor.getEncoder().setPositionConversionFactor(180./108.);
+        turretMotor.setSoftLimit(SoftLimitDirection.kReverse, 0);
+        turretMotor.setSoftLimit(SoftLimitDirection.kForward, 180);
+        turretMotor.enableSoftLimit(SoftLimitDirection.kReverse, true);
+        turretMotor.enableSoftLimit(SoftLimitDirection.kForward, true);
+        turretMotor.setIdleMode(IdleMode.kBrake);
     }
 
     public void setTurretSpeed(double voltPct) {
         turretMotor.set(voltPct);
+    }
+
+    public double getAngle() {
+        return turretMotor.getEncoder().getPosition();
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Turret Encoder", getAngle());
     }
 }
